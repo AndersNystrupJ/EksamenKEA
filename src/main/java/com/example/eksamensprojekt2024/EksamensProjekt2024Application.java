0e645a8b4751961643475e496f14b3f4a042d840
NikolajWild <nime0002@stud.kea.dkgit config --global user.email NikolajWildgit config --global user.email nime0002@stud.kea.dk>
2024-11-25 09:32:28 +0100

    Dependencies

---
+++ src/main/java/com/example/eksamensprojekt2024/EksamensProjekt2024Application.java
@@ -0,0 +1,13 @@
+package com.example.eksamensprojekt2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksamensProjekt2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EksamensProjekt2024Application.class, args);
+    }
+
+}
