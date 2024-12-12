9c263866f67b9005fa110bbc0eabf3037cf87c02
AndersNystrupJ <anje0008@stud.kea.dk>
2024-12-12 13:38:10 +0100

    Added activeprofile to test. Changed local version of JDK.

--- src/test/java/com/example/eksamensprojekt2024/EksamensProjekt2024ApplicationTests.java
+++ src/test/java/com/example/eksamensprojekt2024/EksamensProjekt2024ApplicationTests.java
@@ -2,7 +2,9 @@ package com.example.eksamensprojekt2024;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class EksamensProjekt2024ApplicationTests {
 
